c496b2fa58273dd57f5b8649324762d1bda78f11
Patryk Hirny <pathirny@googlemail.com>
2023-11-17 11:01:19 +0000

    added components folder

--- travelblog/src/app/page.js
+++ travelblog/src/app/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./header";
+import Header from "./components/header";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Link href="/whatever">Click</Link>
+      <Link href="./AddPost">Click</Link>
     </>
   );
 }
