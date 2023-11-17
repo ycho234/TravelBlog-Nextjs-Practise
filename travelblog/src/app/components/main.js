d0872ad3b62c9241d6962f793c8deb7439d15e00
Patryk Hirny <pathirny@googlemail.com>
2023-11-17 14:42:28 +0000

    working scrollable display

--- travelblog/src/app/components/main.js
+++ travelblog/src/app/components/main.js
@@ -1,7 +1,19 @@
+import travelBlog from "@/data";
+import { useState } from "react";
+const travel = travelBlog.map((data) => {
+  return (
+    <div className="elements">
+      <h1 className="title">{data.title}</h1>
+      <img src={data.image}></img>
+      <p className="description">{data.description}</p>
+    </div>
+  );
+});
+
 export default function Main() {
   return (
     <div className="mainContainer">
-      <main className="main"></main>
+      <main className="main">{travel}</main>
     </div>
   );
 }
--- travelblog/src/app/page.js
+++ travelblog/src/app/page.js
@@ -20,7 +20,6 @@ export default function Home() {
   // };
   return (
     <>
-      <h1> {travel.title}</h1>
       <Header />
       <Main />
       <Footer />
