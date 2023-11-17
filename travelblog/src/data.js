b70a6919161555f9d4cb6f39477406b6e6fa8b09
yusra-aina <yusrachoudhury123@gmail.com>
2023-11-17 10:21:19 +0000

    data file

---
+++ travelblog/src/data.js
@@ -0,0 +1,84 @@
+import { v4 as uuidv4 } from "uuid";
+
+travelBlog = [
+  {
+    id: uuidv4(),
+    title: "Japan",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/97/39/7f/caption.jpg?w=1200&h=-1&s=1&cx=1920&cy=1080&chk=v1_f31158e4bb953d28a308",
+    description:
+      "My time in Japan was unforgettable! From the bustling streets of Tokyo to the serene beauty of Kyoto, every moment was filled with wonder. The blend of tradition and modernity, along with the delicious cuisine, made this trip a dream come true.",
+  },
+  {
+    id: uuidv4(),
+    title: "Italy",
+    image:
+      "https://media1.popsugar-assets.com/files/thumbor/W7xBMooTKm4JRstd4jojSYBoEqY=/0x0:7952x5304/fit-in/792x528/top/filters:format_auto():upscale()/2020/03/24/559/n/45101125/06c61ac25e79fc30a64224.30513518_.jpg",
+    description:
+      "Exploring the historical landmarks of Italy was like stepping into a living museum. The art, culture, and architecture left me speechless. The warmth of the people and the incredible food added an extra layer of magic to my journey.",
+  },
+  {
+    id: uuidv4(),
+    title: "Australia",
+    image:
+      "https://i.natgeofe.com/k/b76526f3-bb84-489d-b229-56bcc08aa915/australia-sydney-opera-house.jpg?w=1084.125&h=611.625",
+    description:
+      "Australia's vast landscapes and diverse wildlife took my breath away. From snorkeling in the Great Barrier Reef to hiking in the Outback, every moment was filled with adventure. The friendly locals and stunning natural beauty made it an unforgettable experience.",
+  },
+  {
+    id: uuidv4(),
+    title: "France",
+    image:
+      "https://www.state.gov/wp-content/uploads/2023/07/shutterstock_667548661v2.jpg",
+    description:
+      "Paris, the city of lights, stole my heart! The iconic landmarks like the Eiffel Tower and Louvre Museum were even more impressive in person. The charming streets, delectable pastries, and rich history made every day in France a delight.",
+  },
+  {
+    id: uuidv4(),
+    title: "Peru",
+    image:
+      "https://deih43ym53wif.cloudfront.net/large_machu-picchu-peru-shutterstock_1044047035_a66ac3ba1f.jpeg",
+    description:
+      "Exploring the ancient ruins of Machu Picchu was a dream come true. The breathtaking views from the Andes Mountains and the rich cultural heritage of Peru left me in awe. The vibrant markets and delicious Peruvian cuisine added a beautiful touch to the experience.",
+  },
+  {
+    id: uuidv4(),
+    title: "Thailand",
+    image:
+      "https://static.independent.co.uk/2022/06/17/16/iStock-178749367%20%281%29.jpg",
+    description:
+      "Thailand's vibrant culture and stunning landscapes were a feast for the senses. From the bustling markets of Bangkok to the serene beaches of Phuket, every moment was an adventure. The warmth of the Thai people made me feel at home.",
+  },
+  {
+    id: uuidv4(),
+    title: "Iceland",
+    image:
+      "https://cdn.britannica.com/71/73371-050-9DFAEC1E/Reykjavik-Iceland.jpg",
+    description:
+      "Iceland's dramatic landscapes of glaciers, hot springs, and volcanoes were simply breathtaking. Exploring the Golden Circle and witnessing the Northern Lights was a surreal experience. The raw beauty of Iceland left me in awe.",
+  },
+  {
+    id: uuidv4(),
+    title: "Brazil",
+    image:
+      "https://www.intrepidtravel.com/v3/assets/blt0de87ff52d9c34a8/blt84e6f613930bd904/63c9c76e75a0df5fb06eec98/Intrepid_Travel-brazil_rio-de-janeiro_botafogo-bay-aerial_ss-435627745_2018.jpg",
+    description:
+      "The vibrant energy of Brazil was infectious! From the Carnival celebrations to the stunning Amazon rainforest, every part of this country was alive with culture and natural beauty. The music, food, and friendly people made it an unforgettable journey.",
+  },
+  {
+    id: uuidv4(),
+    title: "South Africa",
+    image:
+      "https://static.independent.co.uk/2022/04/01/16/iStock-477451698.jpg",
+    description:
+      "Exploring the diverse landscapes and wildlife of South Africa was a true adventure. From safaris in Kruger National Park to the stunning views along the Garden Route, every moment was filled with wonder. The rich history and warm hospitality made it an incredible experience.",
+  },
+  {
+    id: uuidv4(),
+    title: "New Zealand",
+    image:
+      "https://www.thetravellingtom.com/wp-content/uploads/2018/11/Mt-Cook-1024x682.jpg.webp",
+    description:
+      "New Zealand's stunning natural beauty and outdoor adventures were unparalleled. From hiking in Fiordland National Park to exploring the geothermal wonders in Rotorua, every corner of this country felt like a paradise for nature lovers.",
+  },
+];
