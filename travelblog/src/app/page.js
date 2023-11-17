4f555a059f96814556c55377fc30f3ac9f798da0
Patryk Hirny <pathirny@googlemail.com>
2023-11-17 12:02:47 +0000

    added pseudo

--- travelblog/src/app/page.js
+++ travelblog/src/app/page.js
@@ -2,10 +2,11 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import Main from "./components/main";
 
-// functionality: 
-//
-
-
+// functionality:
+// display the data.js file (travelBlog)
+// can use a map function or for each function
+// need a container for the Title, image and description
+// for each item create the above
 
 export default function Home() {
   return (
