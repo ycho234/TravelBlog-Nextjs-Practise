b00e601a2fc12a9a9a2e819335c3b12d6027c1f4
Thomas Chappell <tom.chappell.info@gmail.com>
2023-11-17 13:33:35 +0000

    added form component

---
+++ travelblog/src/app/components/form.js
@@ -0,0 +1,23 @@
+
+export default function Form(){
+    return(
+    <>
+        <label>Title</label>
+       <input type="text" />
+       <label>Description</label>
+       <input type="text" />
+       <label>Image</label>
+       <input type="text" />
+
+       <button type="submit">Submit</button>
+    </>
+    )
+}
+
+ // track the input in the input field using state
+
+ // nested function that tracks the string info put into the input field
+
+ // function to handle the submit button (onClick)
+
+ // 
