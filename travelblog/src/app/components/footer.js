80de583de0adaaf53e06f1b87586066510d0bc19
yusra-aina <yusrachoudhury123@gmail.com>
2023-11-17 11:41:31 +0000

    footer

---
+++ travelblog/src/app/components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <h3 ClassName="footer">Created By ReactRangers </h3>
+    </footer>
+  );
+}
--- travelblog/src/app/page.js
+++ travelblog/src/app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/header";
+import Footer from "./components/footer";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Header />
-      
+      <Footer />
     </>
   );
 }
